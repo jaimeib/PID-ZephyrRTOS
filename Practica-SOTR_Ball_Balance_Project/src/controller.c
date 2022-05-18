96d570f13ebc008175c813297741468af059fcc4
Jaime Iglesias <jaimeiglesias@outlook.com>
2022-05-18 14:02:09 +0200

    Update controller.c

--- Practica-SOTR_Ball_Balance_Project/src/controller.c
+++ Practica-SOTR_Ball_Balance_Project/src/controller.c
@@ -10,7 +10,6 @@
 // HAL API
 #include <stm32f7xx_hal.h>
 #include <stm32f7xx_hal_adc.h>
-#include "HAL_ADC.h"
 
 //Modules
 #include <misc/error_checks.h>
@@ -25,6 +24,82 @@
 #define KP -0.08
 #define KI 0 // -0.00001
 
+/* ADC handler declaration */
+static ADC_HandleTypeDef AdcHandle;
+
+//ADC FUNCTIONS:
+static void ADC_Config(void);
+static void Error_Handler(void);
+static void SystemClock_Config(void);
+static void ADC_Select_CH1(void);
+
+static void SystemClock_Config(void)
+{
+	__HAL_RCC_ADC1_CLK_ENABLE(); // Habilita el reloj ADC1
+}
+
+static void ADC_Select_CH1(void)
+{
+	ADC_ChannelConfTypeDef sConfig = { 0 };
+	/* Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
+	sConfig.Channel = ADC_CHANNEL_4;
+	sConfig.Rank = 1;
+	sConfig.SamplingTime = ADC_SAMPLETIME_56CYCLES;
+	sConfig.Offset = 0;
+	if (HAL_ADC_ConfigChannel(&AdcHandle, &sConfig) != HAL_OK) {
+		Error_Handler();
+	}
+}
+
+/**
+  * @brief  Configure the ADC.
+  * @param  None
+  * @retval None
+  */
+static void ADC_Config(void)
+{
+	/* Configure the ADC peripheral */
+	AdcHandle.Instance = ADC1;
+	AdcHandle.Init.ClockPrescaler = ADC_CLOCKPRESCALER_PCLK_DIV4;
+	AdcHandle.Init.Resolution = ADC_RESOLUTION_12B;
+	AdcHandle.Init.ScanConvMode =
+		ENABLE; /* Sequencer disabled (ADC conversion on only 1 channel: channel set on rank 1) */
+	AdcHandle.Init.ContinuousConvMode =
+		ENABLE; /* Continuous mode enabled to have continuous conversion */
+	AdcHandle.Init.DiscontinuousConvMode =
+		DISABLE; /* Parameter discarded because sequencer is disabled */
+	AdcHandle.Init.NbrOfDiscConversion = 0;
+	AdcHandle.Init.ExternalTrigConvEdge =
+		ADC_EXTERNALTRIGCONVEDGE_NONE; /* Conversion start not trigged by an external event */
+	AdcHandle.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+	AdcHandle.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+	AdcHandle.Init.NbrOfConversion = 1;
+	AdcHandle.Init.DMAContinuousRequests = DISABLE;
+	AdcHandle.Init.EOCSelection = DISABLE;
+
+	if (HAL_ADC_Init(&AdcHandle) != HAL_OK) {
+		/* ADC initialization Error */
+		Error_Handler();
+	}
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+static void Error_Handler(void)
+{
+	while (1) {
+		//Print error message
+		printf("Error...");
+
+		// Sleep 20 ms
+		HAL_Delay(20);
+	}
+}
+
 static uint16_t dist_input_gui;
 static uint16_t distance = (CONTROLLER_DIST_NEAR - CONTROLLER_DIST_FAR) / 2;
 static int angle;
@@ -37,8 +112,8 @@ static int distance_mean()
 	uint32_t measure = 0;
 	for (int i = 0; i < NUM_MEASUREMENTS; i++) {
 		do {
-			// Select ADC_CH0
-			ADC_Select_CH0();
+			// Select ADC_CH1
+			ADC_Select_CH1();
 			HAL_ADC_Start(&AdcHandle);
 			HAL_ADC_PollForConversion(&AdcHandle, 1000);
 			measure = HAL_ADC_GetValue(&AdcHandle);
@@ -106,8 +181,8 @@ static void *controller_thread_body(void *arg)
 	// Hal API
 	GPIO_InitTypeDef GPIO_InitStruct;
 
-	// Pin A0 is in PA6, so we need to set it up!
-	GPIO_InitStruct.Pin = GPIO_PIN_6;
+	// Pin A1 is in PA4, so we need to set it up!
+	GPIO_InitStruct.Pin = GPIO_PIN_4;
 	GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
 	GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
 	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
