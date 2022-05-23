a65fb24a66871a029c3d0ef9a2739308a323b074
Jaime Iglesias <jaimeiglesias@outlook.com>
2022-05-23 22:20:41 +0200

    Modificados valores limite del supervisor para la prueba en video

--- Practica-Monitorizacion/include/supervisor.h
+++ Practica-Monitorizacion/include/supervisor.h
@@ -1,8 +1,8 @@
 /* SUPERVISOR CONFIGURATION*/
 
 // THRESHOLD VALUES
-#define LOW_LIGHT_VALUE 10
-#define HIGH_TEMP_VALUE 30
+#define LOW_LIGHT_VALUE 20
+#define HIGH_TEMP_VALUE 25
 
 //Supervisor FUNCTIONS:
 void supervisor(void *ptr_result);
